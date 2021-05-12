ae5f4a333c6d06a7d3c7a685b42bec779314ab26
Vusal Akbarov <vusal.akbarli@hotmail.com>
2021-05-12 15:19:22 +0200

    GUI ist erstellt. 1. Schritt -Auftrag Controller und Views erstellt.

---
+++ OrderOverview/OrderOverview.WebApp/Controllers/AssignmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderOverview.WebApp.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        // GET: Assignment
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
